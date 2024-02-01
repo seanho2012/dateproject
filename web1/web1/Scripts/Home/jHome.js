b06dbeea2da2675866b4f38cc088cfa2de269ce2
GSS_DOMAIN\sean_ho <sean_ho@gss.com.tw>
2024-02-01 17:42:30 +0800

    Description: 新增:新增行程功能
    Issue:
    kendo scheduler新增項目時，舊項目不見
    
    Cause:
    kendo scheduler的eventTemplate需要resources資料用來顯示
    新增項目時resources為空，kendo scheduler掛掉
    
    Resolution:
    resources有資料時才載入

--- web1/web1/Scripts/Home/jHome.js
+++ web1/web1/Scripts/Home/jHome.js
@@ -1,27 +1,95 @@
 ﻿$(function () {
+    var schedulerTemplate = kendo.template("\
+        <div> Title: #: title #</div> \
+        <div>Atendees: \
+            #if(resources.length > 0){#\
+                #: resources[0].text #\
+            #}#\
+        </div>\
+    ");
+
     $("#scheduler").kendoScheduler({
-        date: new Date("2013/6/6"),
-        allDayEventTemplate: $("#event-template").html(),
-        dataSource: [
-            {
-                id: 1,
-                start: new Date("2013/6/6 08:00 AM"),
-                end: new Date("2013/6/6 09:00 AM"),
-                isAllDay: true,
-                title: "Interview",
-                attendees: [1, 2]
-            }
+        date: new Date(),
+        eventTemplate: schedulerTemplate,
+        views: [
+            "day",
+            { type: "week", selected: true },
+            "workWeek",
+            "month",
+            "agenda",
         ],
+        dataSource: {
+            transport: {
+                read: {
+                    type: "post",
+                    dataType: "json",
+                    url: "/Scheduler/GetSchedulerData",
+                    data: function () {
+                        return { "title": "front" }
+                    }
+                },
+                create: {
+                    type: "post",
+                    dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+                    url: "/Scheduler/CreateSchedulerData",
+                    data: function (data) {
+                        console.log("data", data);
+                        var scheduler = {
+                            SchedulerID: data.id,
+                            StartTime: data.start.toJSON(),
+                            EndTime: data.end.toJSON(),
+                            Title: data.title,
+                            LocationID: data.location
+                        };
+                        console.log("scheduler", scheduler);
+                        return scheduler;
+                    }
+                },
+                update: function (e) {
+                    console.log("update:");
+                },
+                destroy: function (e) {
+                    console.log("destroy:");
+                }
+            },
+            schema: {
+                parse: function (response) {
+                    var schedulers = [];
+                    for (var i = 0; i < response.length; i++) {
+                        var scheduler = {
+                            id: response[i].SchedulerID,
+                            start: response[i].StartTime,
+                            end: response[i].EndTime,
+                            title: response[i].Title,
+                            location: response[i].LocationID,
+                            location2: response[i].LocationID
+                        };
+                        schedulers.push(scheduler);
+                    }
+                    return schedulers;
+                }
+            }
+        },
         resources: [
             {
-                field: "attendees",
+                field: "location",
                 dataSource: [
                     { value: 1, text: "Alex" },
                     { value: 2, text: "Bob" }
-                ],
-                multiple: true
-            }
-        ]
+                ]
+            },
+            {
+                field: "location2",
+                dataSource: [
+                    { value: 1, text: "Alex" },
+                    { value: 2, text: "Bob" }
+                ]
+            },
+        ],
+        save: function (e) {
+            var dataSource = $("#scheduler").data("kendoScheduler").dataSource;
+            dataSource.read();
+        }
     });
     var app = angular.module('myApp', []);
     app.controller('myCtrl', function ($scope) {
